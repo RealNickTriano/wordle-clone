174c5d0e3f53f9930dc8b946cf92ff1e2d0614cf
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2022-06-30 13:05:37 -0400

    darkmode compatibility issues

--- src/components/GameListItem.js
+++ src/components/GameListItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GameListItem = ({ title, img, link }) => {
   return (
-    <a href={link} className='flex justify-start pl-8 py-2 items-center gap-2 hover:bg-gray-100 w-full'>
+    <a href={link} className='flex justify-start pl-8 py-2 items-center gap-2 hover:bg-gray-100 dark:hover:bg-neutral-700 w-full'>
         <img src={img} alt={title} className='w-6 p-[1px] border-2 border-black rounded-md' />
         <h1 className='text-sm font-medium pokemon-font'>{title}</h1>
     </a>
